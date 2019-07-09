bd1f01daf1e80708aea8030e0d167528d89407db
Karen <karen.aguilar@hixsa.com>
2019-07-09 14:18:47 -0500

    agregando todo otra vez xD

---
+++ models/Registro.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+const registroSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    nombreUsuario: {
+        type: String,
+        required: true
+    },  
+    //ESTE ES EL HASH GENERADO CON EL PASSWORD Y EL SALT  
+    password: {
+        type: String,
+        required: true
+    },
+    //ESTE ES EL SALT
+    hash: {
+        type: String
+    }
+    }, {timestamps: true});
+
+
+const Registro = mongoose.model('Registro', registroSchema);
+module.exports = {Registro};

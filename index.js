9b5a0ab9f8cad5fe27ec1b0e85f74e41757a4e68
Karen <karen.aguilar@hixsa.com>
2019-07-10 17:17:03 -0500

    env

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { Registro } = require('./models/Registro')
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 
-require('dotenv').config();
+require('env').config();
 
 const PORT = process.env.PORT || 3001;
 
